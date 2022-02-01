cac3c70a1d54e3b116f11561c797a01edf7b7080
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2022-02-01 19:25:58 +0530

    rewrite

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,9 @@
 import type { NextPage } from "next";
-import Aboutme from "../components/Aboutme";
-import Hero from "../components/Hero";
-import Nav from "../components/Nav";
-import Skills from "../components/Skills";
-import Testimonial from "../components/Testimonial";
-import Toggleup from "../components/Toggleup";
-import Contact from "../components/Contact";
-import Footer from "../components/Footer";
-import Projects from "../components/Projects";
 
 const Home: NextPage = () => {
   return (
     <div className="min-h-screen min-w-screen bg-mainbg">
-      <Toggleup />
-      <Nav />
-
-      <Hero />
-      <Aboutme />
-      <Skills />
-      <Projects />
-      <Testimonial />
-      <Contact />
-      <Footer />
+      Hello
     </div>
   );
 };
