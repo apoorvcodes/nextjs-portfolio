9291a994e1933471e59bdda4657e3bee5b51836e
Apoorv <tofu.develops@gmail.com>
2022-05-17 13:13:34 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import type { NextPage } from "next";
 import Aboutme from "../components/Aboutme";
 import Hero from "../components/Hero";
-import Nav from "../components/Nav";
 import Testimonial from "../components/Testimonial";
 import Toggleup from "../components/Toggleup";
 import Contact from "../components/Contact";
-import Footer from "../components/Footer";
 import Projects from "../components/Projects";
 import Head from "next/head";
 const Home: NextPage = () => {
@@ -18,13 +16,11 @@ const Home: NextPage = () => {
         ></link>
       </Head>
       <Toggleup />
-      <Nav />
       <Hero />
       <Aboutme />
       <Projects />
       <Testimonial />
       <Contact />
-      <Footer />
     </div>
   );
 };
